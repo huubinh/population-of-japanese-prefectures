a9e0d13a357d17b2e14f40ab49d15b2e69a681b2
Binh Tran <tranhuubinh223@gmail.com>
2022-02-05 17:42:46 +0700

    Create react app and set up the folder structure

---
+++ src/app/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "../components/Header";
+import Checkboxes from "../components/Checkboxes";
+import LineChart from "../components/LineChart";
+import Footer from "../components/Footer";
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<Checkboxes />
+			<LineChart />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Checkboxes/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Checkbox from "../Checkbox";
+
+function Checkboxes() {
+	return (
+		<div>
+			<Checkbox />
+			<Checkbox />
+			<Checkbox />
+		</div>
+	);
+}
+
+export default Checkboxes;
